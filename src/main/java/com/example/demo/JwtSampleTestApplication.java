77f673ae85bf662e586a413d4235d92a3c302fc8
Saikat Chakraborty <saikat.chakraborty1@publicissapient.com>
2022-07-19 22:45:39 +0530

    1st Commit

---
+++ src/main/java/com/example/demo/JwtSampleTestApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSampleTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSampleTestApplication.class, args);
+    }
+}
